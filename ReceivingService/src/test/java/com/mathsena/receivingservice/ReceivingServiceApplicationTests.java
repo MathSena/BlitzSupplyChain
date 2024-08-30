29ed92a2c16dd5eb18e2395d086c5ec755c138f4
MathSena <MathSena07@hotmail.com>
2024-08-30 01:55:56 +0100

    Initial commit

---
+++ ReceivingService/src/test/java/com/mathsena/receivingservice/ReceivingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.receivingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceivingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
